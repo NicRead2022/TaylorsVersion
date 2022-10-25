f4a7bfd3fd9e7b76ac7d2dbbb6a1ec060928cf99
Nicole Read <nicole.poulard@gmail.com>
2022-10-25 16:45:43 -0400

    because matt said so

--- controllers/index.js
+++ controllers/index.js
@@ -18,13 +18,54 @@ const createSong = async (req, res) => {
         return res.status(500).json({ error: error.message })
     }
 }
-const readBoyfriend = async (req,res) => {
+const getBoyfriendById = async (req,res) => {
     try {
-        // const boyfriend = await Boyfriend.find({name:req.Boyfriend.})
+        const boyfriend = await Boyfriend.findById(req.params.id)
+        return res.status(201).json(boyfriend);
+} catch (error) {
+    return res.status(500).json({ error: error.message })
+}
+}
+const getAllBoyfriends = async (req,res) => {
+    try {
+        const boyfriends = await Boyfriend.find()
+        return res.status(201).json(boyfriends);
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const getAllSongs = async (req,res) => {
+    try {
+        const songs = await Song.find()
+        return res.status(201).json(songs);
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+const getSongById = async (req,res) => {
+    try {
+        const song = await Song.findById(req.params.id)
+        return res.status(201).json(song);
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+const getSongsByBoyfriend = async (req,res) => {
+    try {
+        const song = await Song.find({boyfriend:req.params.id})
+        return res.status(201).json(song);
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
     }
 }
 
 module.exports = {
     createBoyfriend,
-    createSong
+    createSong,
+    getAllSongs,
+    getSongById,
+    getBoyfriendById,
+    getAllBoyfriends,
+    getSongsByBoyfriend,
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const controllers = require('../controllers')
 const router = Router();
 
 
-router.post('/boyfriends',controllers.createBoyfriend)
+router.post('/boyfriends', controllers.createBoyfriend)
 router.post('/songs', controllers.createSong)
+router.get('/boyfriends', controllers.getAllBoyfriends)
+router.get('/boyfriends/:id', controllers.getBoyfriendById)
+router.get('/songs', controllers.getAllSongs)
+router.get('/songs/:id', controllers.getSongById)
+router.get('/boyfriends/:id/songs', controllers.getSongsByBoyfriend)
 
 module.exports = router
